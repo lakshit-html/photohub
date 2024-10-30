89a6634d8578b9b4a1102c8dd2c7ab0261175140
lakshit-html <lakshitsolanki1234@gmail.com>
2024-10-30 13:44:04 +0530

    hello

---
+++ learn.js
@@ -0,0 +1,17 @@
+let gameNum= 25 ;
+let userNum= prompt("Guss the Game Number:");
+// console.log(userNum)
+// if (userNum==gameNum){
+//     console.log("Right Guss")
+// }
+// else if (userNum!=gameNum){
+//     console.log("        Wrong Guss")
+//     let userNum= prompt("guss the number:") 
+ 
+// }
+while(userNum!=gameNum){
+    // let userNum= prompt("guss the number:")
+ userNum =   prompt("Guss the Game Number:");
+}
+
+document.write("Right Guss")
